c990befe05244d675a7fa892cf365975cb73f85a
kakakaeng <kakakaeng@gmail.com>
2023-02-23 23:24:57 +0700

    build(shared-module): init shared module

--- nasa-astronomy-gallery/src/app/app.module.ts
+++ nasa-astronomy-gallery/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
