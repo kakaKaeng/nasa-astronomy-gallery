d168a0e969e981ddcc2c4a025de4d4342c9e8dd8
kakakaeng <kakakaeng@gmail.com>
2023-02-23 22:25:22 +0700

    feat(navbar): navbar

--- nasa-astronomy-gallery/src/app/core/core.module.ts
+++ nasa-astronomy-gallery/src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ScrollDirective } from './navbar/directives/scroll.directive';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ScrollDirective
   ],
   imports: [
     CommonModule
