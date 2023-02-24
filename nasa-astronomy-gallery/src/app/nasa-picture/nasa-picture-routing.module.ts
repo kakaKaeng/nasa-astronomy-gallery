5cca178f5e1dd5ce507ba4c3f2b4bb0bad934ba1
kakakaeng <kakakaeng@gmail.com>
2023-02-24 16:53:52 +0700

    feat(picture-list): init component

--- nasa-astronomy-gallery/src/app/nasa-picture/nasa-picture-routing.module.ts
+++ nasa-astronomy-gallery/src/app/nasa-picture/nasa-picture-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DailyPictureComponent } from './daily-picture/daily-picture.component';
+import { PictureListComponent } from './picture-list/picture-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DailyPictureComponent
+  },
+  {
+    path: 'search',
+    component: PictureListComponent
   }
 ];
 
--- nasa-astronomy-gallery/src/app/nasa-picture/nasa-picture.module.ts
+++ nasa-astronomy-gallery/src/app/nasa-picture/nasa-picture.module.ts
@@ -5,11 +5,13 @@ import { NasaPictureRoutingModule } from './nasa-picture-routing.module';
 import { DailyPictureComponent } from './daily-picture/daily-picture.component';
 import { SharedModule } from '../shared/shared.module';
 import { LAZYLOAD_IMAGE_HOOKS, LazyLoadImageModule, ScrollHooks } from 'ng-lazyload-image';
+import { PictureListComponent } from './picture-list/picture-list.component';
 
 
 @NgModule({
   declarations: [
-    DailyPictureComponent
+    DailyPictureComponent,
+    PictureListComponent
   ],
   imports: [
     CommonModule,
